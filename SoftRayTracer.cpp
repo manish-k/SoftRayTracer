a254c04f4f56c8296249c8c04f502f1fa919da81
Manish Kaushal <manishkaushal163@gmail.com>
2025-04-01 17:20:42 +0530

    Add project files.

---
+++ SoftRayTracer.cpp
@@ -0,0 +1,12 @@
+﻿// SoftRayTracer.cpp : Defines the entry point for the application.
+//
+
+#include "SoftRayTracer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
